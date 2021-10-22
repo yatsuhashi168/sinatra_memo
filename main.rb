f40541129e34826abd3b106f83432065f5f0b52e
yatsuhashi168 <kurimi777luk@gmail.com>
2021-10-22 16:47:19 +0900

    /memosにメモを連番で保存できるようにした

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
   erb :index
@@ -7,6 +8,21 @@ end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+  title =  params[:name]
+  content = params[:content]
+  id = if Dir.empty?('./memos')
+         1
+       else
+         Dir.glob('*', base: 'memos').last[/\d{1,}/].to_i + 1
+       end
+  memo = {'id' => id, 'title' => title, 'content' => content}
+  File.open("./memos/memo_#{id}.json", 'w') do |file|
+    JSON.dump(memo, file)
+  end
+  redirect to('/')
 end
 
 get '/detail' do
