1f27fbd3728ff37185462e2a802d3262f6098259
yatsuhashi168 <kurimi777luk@gmail.com>
2021-10-25 16:37:30 +0900

    メモ作成画面と一覧画面にタイトルを追加 一覧画面のビュー作成（詳細画面へのリンクなし）

--- main.rb
+++ main.rb
@@ -3,10 +3,18 @@ require 'sinatra/reloader'
 require 'json'
 
 get '/' do
+  @title = '一覧'
+  @memos = []
+  Dir.glob('*', base: 'memos').each do |file|
+    File.open("./memos/#{file}") do |memo|
+      @memos << JSON.load(memo)['title']
+    end
+  end
   erb :index
 end
 
 get '/new' do
+  @title = '新規作成'
   erb :new
 end
 
@@ -16,7 +24,7 @@ post '/new' do
   id = if Dir.empty?('./memos')
          1
        else
-         Dir.glob('*', base: 'memos').last[/\d{1,}/].to_i + 1
+         Dir.glob('*', base: 'memos').last[/\d+/].to_i + 1
        end
   memo = {'id' => id, 'title' => title, 'content' => content}
   File.open("./memos/memo_#{id}.json", 'w') do |file|
@@ -32,5 +40,3 @@ end
 get '/edit' do
   erb :edit
 end
-
-
