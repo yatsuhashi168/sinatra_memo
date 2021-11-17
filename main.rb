41722264e8a640ed4a4c34fdd72f9546d9bf8073
yatsuhashi168 <kurimi777luk@gmail.com>
2021-11-17 14:43:27 +0900

    メモ編集画面のルーティングをDB仕様に変更

--- main.rb
+++ main.rb
@@ -97,11 +97,20 @@ patch '/memo/:id' do
   redirect to("/memo/#{params[:id]}")
 end
 
+# get '/memo/:id/edit' do
+#   @memo = JSON.parse(File.read("./memos/memo_#{params[:id]}.json"))
+#   @title = @memo['name']
+#   @memo_id = @memo['id']
+#   @memo_name = @memo['name']
+#   @memo_content = @memo['content']
+#   erb :edit
+# end
+
 get '/memo/:id/edit' do
-  @memo = JSON.parse(File.read("./memos/memo_#{params[:id]}.json"))
-  @title = @memo['name']
-  @memo_id = @memo['id']
-  @memo_name = @memo['name']
-  @memo_content = @memo['content']
+  @memo = PG.connect(dbname: 'sinatra_memo').exec("SELECT * FROM memos WHERE id = #{params[:id]}")
+  @title = @memo[0]['name']
+  @memo_id = @memo[0]['id']
+  @memo_name = @memo[0]['name']
+  @memo_content = @memo[0]['content']
   erb :edit
 end
