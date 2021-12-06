3d26f637f57b5a781a74f5d559fa01ef2ddc75b8
yatsuhashi168 <kurimi777luk@gmail.com>
2021-12-06 14:22:01 +0900

    データベース接続のメソッド名を`connecting_database`に変更

--- main.rb
+++ main.rb
@@ -12,14 +12,14 @@ helpers do
     Rack::Utils.escape_html(text)
   end
 
-  def connect_database
+  def connecting_database
     @connection ||= PG.connect(dbname: 'sinatra_memo')
   end
 end
 
 get '/' do
   @title = '一覧'
-  @memos = connect_database.exec('SELECT * FROM memos ORDER BY id') do |results|
+  @memos = connecting_database.exec('SELECT * FROM memos ORDER BY id') do |results|
     results.map do |memo|
       memo
     end
@@ -33,7 +33,7 @@ get '/memo/new' do
 end
 
 post '/memo' do
-  connection = connect_database
+  connection = connecting_database
   connection.prepare('new', 'INSERT INTO memos(name, content) VALUES ($1, $2)')
   connection.exec_prepared('new', [params[:name], params[:content]])
 
@@ -41,18 +41,18 @@ post '/memo' do
 end
 
 get '/memo/:id' do
-  @memo = connect_database.exec("SELECT * FROM memos WHERE id = #{params[:id]}")[0]
+  @memo = connecting_database.exec("SELECT * FROM memos WHERE id = #{params[:id]}")[0]
   @title = @memo['name']
   erb :detail
 end
 
 delete '/memo/:id' do
-  connect_database.exec("DELETE FROM memos WHERE id = #{params[:id]}")
+  connecting_database.exec("DELETE FROM memos WHERE id = #{params[:id]}")
   redirect to('/')
 end
 
 patch '/memo/:id' do
-  connection = connect_database
+  connection = connecting_database
   connection.prepare('patch', "UPDATE memos SET name = $1, content = $2 WHERE id = #{params[:id]}")
   connection.exec_prepared('patch', [params[:name], params[:content]])
 
@@ -60,7 +60,7 @@ patch '/memo/:id' do
 end
 
 get '/memo/:id/edit' do
-  @memo = connect_database.exec("SELECT * FROM memos WHERE id = #{params[:id]}")[0]
+  @memo = connecting_database.exec("SELECT * FROM memos WHERE id = #{params[:id]}")[0]
   @title = @memo['name']
   erb :edit
 end
